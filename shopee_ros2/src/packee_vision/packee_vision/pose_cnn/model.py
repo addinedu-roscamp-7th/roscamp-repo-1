1e28138dc492e2f7879c54b5d3728631bc671f2d
songwonjoon <djckorea58@gmail.com>
2025-10-23 19:38:25 +0900

    [packee_vision] CNN 모델 수정

--- shopee_ros2/src/packee_vision/packee_vision/pose_cnn/model.py
+++ shopee_ros2/src/packee_vision/packee_vision/pose_cnn/model.py
@@ -3,26 +3,42 @@ import torch.nn as nn
 from torchvision import models
 
 class PoseCNN(nn.Module):
-    def __init__(self, num_classes=6):
+    def __init__(self, num_classes=3, pose_classes=6):
         super().__init__()
-        resnet = models.resnet18(pretrained=False)
-        self.backbone = nn.Sequential(*list(resnet.children())[:-1])
-        feat_dim = 512
-        self.pose_head = nn.Sequential(
-            nn.Linear(feat_dim, 256),
-            nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(256, 6)
-        )
-        self.class_head = nn.Sequential(
-            nn.Linear(feat_dim, 128),
-            nn.ReLU(),
-            nn.Dropout(p=0.3),
-            nn.Linear(128, num_classes)
+        
+        base = models.resnet18(pretrained=True)
+        self.feature_extractor = nn.Sequential(*list(base.children())[:-1])
+        
+        self.reprogress = nn.Sequential(
+            nn.Linear(512 * 2, 256),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.3),
+            nn.Linear(256, 128),
+            nn.ReLU(inplace=True)
         )
 
-    def forward(self, x):
-        f = self.backbone(x).flatten(1)
-        pose_out = self.pose_head(f)
-        cls_out = self.class_head(f)
-        return pose_out, cls_out
+        self.pose_head = nn.Linear(128, pose_classes)
+
+        self.class_head = nn.Linear(128, num_classes)
+    
+    def forward(self, current_img, target_img):
+        current_feature = self.feature_extractor(current_img).flatten(1)
+        target_feature = self.feature_extractor(target_img).flatten(1)
+
+        feature = torch.cat([current_feature, target_feature], dim=1)
+        shared = self.reprogress(feature)
+
+        pose_output = self.pose_head(shared)
+        class_output = self.class_head(shared)
+        
+
+        return pose_output, class_output
+    
+if __name__ == "__main__":
+    model = PoseCNN()
+    current_img = torch.randn(1, 3, 224, 224)
+    target_img = torch.randn(1, 3, 224, 224)
+
+    pred_pose, pred_class = model(current_img, target_img)
+    print(f"pose shape: {pred_pose.shape}")
+    print(f"class shape: {pred_class.shape}")
