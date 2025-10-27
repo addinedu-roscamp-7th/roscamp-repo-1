8f91e6f465000a8fbb2f98a6b7130f8a9f218376
RHyeJin <tst50087@gmail.com>
2025-10-27 13:44:49 +0900

    [PickeeArm] setup.py의 entry_point를 리팩토링된 노드로 수정

--- shopee_ros2/src/pickee_arm/setup.py
+++ shopee_ros2/src/pickee_arm/setup.py
@@ -24,7 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'pickee_arm_controller = pickee_arm.pickee_arm_controller:main',
+            'pickee_arm_node = pickee_arm.pickee_arm_node:main',
             'full_sequence_client = pickee_arm.full_sequence_client:main',
         ],
     },
