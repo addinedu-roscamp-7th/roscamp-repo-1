7200cb5d6b0298c70508f7bff1180c5b45562636
lim123123 <f499314e@gmail.com>
2025-11-15 14:35:15 +0900

    [PickeeMobile] readme 테스트

---
+++ shopee_ros2/src/pickee_mobile/pickee_mobile/test/etc/단순계산.py
@@ -0,0 +1,13 @@
+# import math
+
+# offset = 10
+# diff = 129
+
+# angle = math.atan(offset/diff)
+# print(math.degrees(angle))
+
+sum = 21.45 + 21.75 + 21.68 + 21.60 + 21.58 + 22.87 + 20.59 + 20.94 + 15.97
+count = 9
+
+average = sum/count
+print(average)
